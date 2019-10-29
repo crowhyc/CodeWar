8589ca10c20969689250a5d2e2391a2f59062071
crowhyc <crowhyc@163.com>
2019-10-29 15:34:46 +0800

    [Add] -  add passed Differentiate a polynomial Quest.

--- src/main/java/com/javacloud/codewar/Equation.java
+++ src/main/java/com/javacloud/codewar/Equation.java
@@ -1,18 +1,89 @@
 package com.javacloud.codewar;
 
+import java.math.BigInteger;
 import java.util.stream.IntStream;
 
 public class Equation {
 
-  public int differentiate(String x, int i) {
-    Integer factor =
-        IntStream.range(0, x.length())
-            .mapToObj(idx -> x.toCharArray()[idx])
-            .filter(ret -> !ret.equals("x"))
-            .map(Object::toString)
-            .map(Integer::parseInt)
-            .findFirst()
-            .orElse(0);
-    return i / factor;
+  public static BigInteger differentiate(String equation, long x) {
+    System.out.println("equation is " + equation + " x is " + x);
+    StringBuffer tempStrBuffer = new StringBuffer();
+    EquationBuffer equationBuffer = new EquationBuffer(x);
+    IntStream.range(0, equation.length())
+        .mapToObj(idx -> equation.toCharArray()[idx])
+        .forEach(
+            character -> {
+              if (character == '+') {
+                equationBuffer.appendNumAndClearBuffer(tempStrBuffer);
+                equationBuffer.setNextPlus(true);
+              } else if (character == '-') {
+                equationBuffer.appendNumAndClearBuffer(tempStrBuffer);
+                equationBuffer.setNextPlus(false);
+              } else {
+                tempStrBuffer.append(character);
+              }
+            });
+    if (tempStrBuffer.indexOf("x") > -1) {
+      equationBuffer.appendNumAndClearBuffer(tempStrBuffer);
+    }
+    return equationBuffer.getResult();
+  }
+
+  static class EquationBuffer {
+    private BigInteger result = BigInteger.ZERO;
+    private BigInteger factorNum;
+    private boolean isNextPlus = true;
+
+    public BigInteger getResult() {
+      return new BigInteger(result + "");
+    }
+
+    public EquationBuffer(long factorNum) {
+      this.factorNum = new BigInteger(factorNum + "");
+    }
+
+    public void add(String factorNum) {
+      result = result.add(getResultNum(factorNum));
+    }
+
+    public void setNextPlus(boolean isPlus) {
+      this.isNextPlus = isPlus;
+    }
+
+    private BigInteger getResultNum(String num) {
+      BigInteger retVal;
+      if (num.equals("x")) {
+        retVal = BigInteger.ONE;
+      } else {
+        int xIndex = num.indexOf("x");
+        if (xIndex == 0) {
+          retVal = BigInteger.ONE;
+        } else {
+          retVal = new BigInteger(num.substring(0, xIndex));
+        }
+        if (num.contains("^")) {
+          int indexOfPower = num.indexOf("^");
+          String powerTimes = num.substring(indexOfPower + 1);
+          retVal =
+              retVal
+                  .multiply(new BigInteger(powerTimes))
+                  .multiply(
+                      factorNum.pow(
+                          new BigInteger(powerTimes).add(BigInteger.ONE.negate()).intValue()));
+        }
+      }
+      if (isNextPlus) {
+        return retVal;
+      } else {
+        return retVal.negate();
+      }
+    }
+
+    public void appendNumAndClearBuffer(StringBuffer tempStrBuffer) {
+      if (tempStrBuffer.length() > 0) {
+        add(tempStrBuffer.toString());
+        tempStrBuffer.delete(0, tempStrBuffer.length());
+      }
+    }
   }
 }
--- src/test/java/com/javacloud/codewar/EquationTest.java
+++ src/test/java/com/javacloud/codewar/EquationTest.java
@@ -1,8 +1,8 @@
 package com.javacloud.codewar;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
 
+import java.math.BigInteger;
 import org.junit.Test;
 
 /**
@@ -13,23 +13,38 @@ import org.junit.Test;
  *
  * <p>Assumptions: There will be a coefficient near each x, unless the coefficient equals 1 or -1.
  * There will be an exponent near each x, unless the exponent equals 0 or 1. All exponents will be
- * greater or equal to zero
- * Examples:
- *          Equation.differenatiate("12x+2", 3) ==> 12
- *          Equation.differenatiate("x^2+3x+2", 3) ==> 9
+ * greater or equal to zero Examples: Equation.differenatiate("12x+2", 3) ==> 12
+ * Equation.differenatiate("x^2+3x+2", 3) ==> 9
  */
 public class EquationTest {
   private Equation equation = new Equation();
 
   @Test
-  public void shouldEquationDifferentiateReturn5_whenInputPolynomialXAndEquals5() {
-    int result = equation.differentiate("x", 5);
-    assertThat(result, is(5));
+  public void shouldEquationDifferentiateReturn12_whenInputPolynomial12xPlus2And3() {
+    BigInteger result = equation.differentiate("12x+2", 3);
+    assertEquals(result, new BigInteger("12"));
   }
 
   @Test
-  public void shouldReturn5_whenInput5XAnd25() {
-    int result = equation.differentiate("5x", 25);
-    assertThat(result, is(5));
+  public void shouldEquationDifferentiateReturn5_whenInputPolynomialxPower2And3() {
+
+    BigInteger result = equation.differentiate("x^2-x", 3);
+    assertEquals(result, new BigInteger("5"));
+  }
+
+  @Test
+  public void
+      shouldEquationDifferentiateReturnMinus20_whenInputPolynomialMinus5xPower2Plus10xPlus4And3() {
+
+    BigInteger result = equation.differentiate("-5x^2+10x+4", 3);
+    assertEquals(result, new BigInteger("-20"));
+  }
+
+  @Test
+  public void
+  shouldEquationDifferentiateReturnMinus105_whenInputPolynomialMinus5xPower3Plus10xPlus4And3() {
+
+    BigInteger result = equation.differentiate("-5x^3+10x+4", 3);
+    assertEquals(result, new BigInteger("-125"));
   }
 }
